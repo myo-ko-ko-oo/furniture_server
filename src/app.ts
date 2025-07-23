ec737cb25c7848922edc63297b71ff4cc80ee410
MYO KO KO OO <myokokooo.5287@gmail.com>
2025-07-23 11:04:05 +0630

    setup prisma & db

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
@@ -12,9 +12,20 @@ app
   .use(compression()) // to compress small files when req,res time quickly
   .use(cors()) // to allow origin domain eg.[www.mysite.com/api,www.frontend.com]
   .use(express.json())
-  .use(helmet())
-  .use(limiter); //to manage header{security ,suh as token}
+  .use(helmet()) //to manage header{security ,suh as token}
+  .use(limiter);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello testing" });
+  res
+    .status(200)
+    .send(
+      "<h1>Furniture Tutorial Server </h1></br><h3>server is running.....</h3>"
+    );
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  const status = error.status || 500;
+  const message = error.message || "Server Error";
+  const errorCode = error.code || "Error_Code";
+  res.status(status).json({ message, error: errorCode });
 });
